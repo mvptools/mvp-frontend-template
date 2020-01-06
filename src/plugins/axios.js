f16d4cb5e44e22a2898b2f589a79ec76e7ee90ab
Gus deMayo <gus.demayo@gmail.com>
2020-01-06 17:59:31 -0600

    token refresh

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
 import isJSON from 'is-json'
+import router from '@/router'
+import store from '@/store'
+import refresh from '@/graphql/refresh.gql'
 
 const axiosInstance = axios.create({
   baseURL: process.env.VUE_APP_API_URL,
@@ -12,7 +15,22 @@ const axiosInstance = axios.create({
 
 axiosInstance.interceptors.response.use(response => {
   if (response.data.errors !== undefined) {
-    return Promise.reject(response.data.errors)
+    if (response.data.errors[0].debugMessage !== 'Unauthenticated.') {
+      return Promise.reject(response.data.errors)
+    }
+
+    axiosInstance({
+      method: 'post',
+      data: {
+        query: refresh
+      }
+    }).then(response => {
+      localStorage.setItem('token', JSON.stringify(response.data.data.refresh))
+    }).catch(() => {
+      store.dispatch('logout').then(() => {
+        router.go()
+      })
+    })
   }
 
   return response
